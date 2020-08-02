ff54a4c8094bf8979c622ab353d0e754233f6296
Jorge Arturo Vasquez Rojas <arturovasq.1234@gmail.com>
2020-08-02 00:30:00 -0600

    AppComponent arreglado

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,4 @@
 import { Component } from '@angular/core';
-import {Router} from '@angular/router'
 
 @Component({
   selector: 'app-root',
@@ -8,11 +7,7 @@ import {Router} from '@angular/router'
 })
 export class AppComponent {
   title = 'Furniture-ecomerce';
+  constructor(){}
 
-  constructor(public router:Router){}
 
- 
-  public iniciarSesion(){
-    this.router.navigateByUrl('/vista-login')
-  }
 }
