784298a7e3f62f486eb5f9a912f3baf1f9facc3f
Bernold <bernoldaz@gmail.com>
2020-08-10 16:48:03 -0600

    un poquito mas

--- src/app/vista-cambiar-estado/vista-cambiar-estado.component.ts
+++ src/app/vista-cambiar-estado/vista-cambiar-estado.component.ts
@@ -19,13 +19,41 @@ export class VistaCambiarEstadoComponent implements OnInit {
 
   public pedidos : Pedido[]= this.compra.pedidos;
 
-  displayedColumns: string[] = ["ID" ,'Correo Cliente', 'Fecha', 'Pago', 'Estado'];
+  displayedColumns: string[] = ["Id" ,'CorreoC',"total" ,  'Fecha', 'Pago', 'Estado' , "Cambiar"];
   dataSource = this.pedidos;
 
   constructor( private compra : CompraService ) { }
 
   ngOnInit(): void {
 
+  }
+
+  pago : any[] = [
+    { Estado : "Cancelado"},
+    { Estado : "Pendiente"}
+  ]
+
+  envio : any[] = [
+    { Estado : "Fabricación"},
+    { Estado : "Almacenado"},
+    { Estado : "Enviado"},
+    { Estado : "Entregado"}
+  ]
+
+  foods: any[] = [
+    {value: 'steak-0', viewValue: 'Steak'},
+    {value: 'pizza-1', viewValue: 'Pizza'},
+    {value: 'tacos-2', viewValue: 'Tacos'}
+  ];
+  selectedFood = this.foods[2].value;
+  selectedPago = "";
+  selectedEnvio = "";
+
+  setSelectedPago ( numero ){
+    this.selectedPago = this.pago[numero].Estado;
+  }
+  setSelectedEnvio( numero ){
+    this.selectedEnvio = this.envio[numero].Estado;
   }
 
 }
